fe6786610c765203be2bef26a41e6c3197b2bd8d
Tran Hoang Vu <vuth@behemoth.vn>
2020-05-08 10:59:11 +0700

    revert

--- index.js
+++ index.js
@@ -1,32 +1,10 @@
-const express = require("express");
-const https = require('https');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const processor = require('./webhook-handler');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-server = https.createServer(app);
-
-app.get("/", (req, res) => {
-  console.log(req)
-  res.send("OK")
-  // res.sendFile("index.html", { root: __dirname });
-});
-
-app.post('/webhook', (req, res) => {
-  // const body = req.body
-  // console.log(req)
-  console.log("requested")
-  processor(req, res)
-})
-
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => {
-  console.log(`Server listing on port ${PORT}`);
-});
-
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
