5777f06fd465c1bc95bf5f1bedd52cc732c0a080
Tran Hoang Vu <vuth@behemoth.vn>
2020-05-08 11:03:23 +0700

    new commit

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const http = require('http');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const processor = require('./webhook-handler');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  console.log(req)
+  res.send("OK")
+  // res.sendFile("index.html", { root: __dirname });
+});
+
+app.post('/webhook', (req, res) => {
+  // const body = req.body
+  // console.log(req)
+  console.log("requested")
+  processor(req, res)
+})
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server listing on port ${PORT}`);
+});
+
